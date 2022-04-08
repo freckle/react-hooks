62b009209909d111eae4bae533db2ea7b2b4ed4b
Restyled.io <commits@restyled.io>
2022-04-08 13:35:33 +0000

    Restyled by prettier

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,19 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {value : true});
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWord = void 0;
 
-var helloWord = function helloWord() { return "Hello, World!"; };
+var helloWord = function helloWord() {
+  return "Hello, World!";
+};
 
 exports.helloWord = helloWord;
-"use strict";
+("use strict");
 
 var _ = require(".");
 
-describe("helloWord", function() {
-  test("is hello world",
-       function() { expect((0, _.helloWord)()).toBe("Hello, World!"); });
+describe("helloWord", function () {
+  test("is hello world", function () {
+    expect((0, _.helloWord)()).toBe("Hello, World!");
+  });
 });
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,7 @@
-import {helloWord} from ".";
+import { helloWord } from ".";
 
 describe("helloWord", () => {
-  test("is hello world", () => { expect(helloWord()).toBe("Hello, World!"); });
+  test("is hello world", () => {
+    expect(helloWord()).toBe("Hello, World!");
+  });
 });
