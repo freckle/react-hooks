d24826b965086883917cf29bbbb94eb539631ae6
Restyled.io <commits@restyled.io>
2022-04-08 13:25:09 +0000

    Restyled by prettier

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {value : true});
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWord = void 0;
 
-var helloWord = function helloWord() { return "Hello, World!"; };
+var helloWord = function helloWord() {
+  return "Hello, World!";
+};
 
 exports.helloWord = helloWord;
