5858e685382dee0b716d0ba9e71df048e9004726
AJ Ruf <z0isch@gmail.com>
2024-10-16 11:21:21 -0400

    dist bump

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,5 +3,5 @@ export type { PrimitiveDep, CallbackFn, ExtraDeps } from './use-extra-deps';
 export { useSafeEffect, useSafeEffectExtraDeps } from './use-safe-effect';
 export { useSafeCallback, useSafeCallbackExtraDeps } from './use-safe-callback';
 export { usePrevious } from './use-previous';
-export { unCallbackFn, unsafeMkCallbackFn, useExtraDeps } from './use-extra-deps';
+export { unsafeMkCallbackFn, useExtraDeps } from './use-extra-deps';
 export { useSafeImperativeHandle, useSafeImperativeHandleExtraDeps } from './use-safe-imperative-handle';
--- dist/use-extra-deps/index.d.ts
+++ dist/use-extra-deps/index.d.ts
@@ -1,16 +1,17 @@
-export declare type PrimitiveDep = boolean | string | number | null | void | symbol;
-export declare type CallbackFn<F> = {
-    callback: F;
+export declare type PrimitiveDep = boolean | string | number | null | void | symbol | CallbackFn<(...v: any) => any>;
+declare const __brand: unique symbol;
+export declare type CallbackFn<F> = F & {
+    [__brand]: "CallbackFn";
 };
-export declare const unCallbackFn: <F>({ callback }: CallbackFn<F>) => F;
 export declare function unsafeMkCallbackFn<F extends (...v: any) => any>(callback: F): CallbackFn<F>;
 export declare type ExtraDeps<V> = {
     value: V;
     comparator: (a: V, b: V) => boolean;
-} | CallbackFn<V>;
+};
 export declare function useExtraDeps<T extends Record<string, unknown>>(deps: ReadonlyArray<PrimitiveDep>, extraDeps: {
     [P in keyof T]: T[P] extends infer R ? ExtraDeps<R> : never;
 }): {
     allDeps: ReadonlyArray<any>;
     extraDepValues: T;
 };
+export {};
